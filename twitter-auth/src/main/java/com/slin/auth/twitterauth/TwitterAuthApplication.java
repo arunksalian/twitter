e784516742afdb83b90319fd4910d1f7d6a86359
Arun Kannan <arkannan@ghx.com>
2019-05-29 16:34:35 +0530

    Auth server

---
+++ twitter-auth/src/main/java/com/slin/auth/twitterauth/TwitterAuthApplication.java
@@ -0,0 +1,13 @@
+package com.slin.auth.twitterauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterAuthApplication.class, args);
+	}
+
+}
