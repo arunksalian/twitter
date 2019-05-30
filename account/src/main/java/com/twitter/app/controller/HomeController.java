a092948e7c16f22a1781eaf4009f1f59c9a0a7ab
Arun Kannan <arkannan@ghx.com>
2019-05-30 20:01:35 +0530

    Logger added

--- account/src/main/java/com/twitter/app/controller/HomeController.java
+++ account/src/main/java/com/twitter/app/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.twitter.app.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/home")
 public class HomeController {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
 	@RequestMapping(method=RequestMethod.GET)
 	@ResponseBody
 	public String getHome () {
+		LOGGER.info("*************HOME***********");
 		return "I am home";
 	}
 	
